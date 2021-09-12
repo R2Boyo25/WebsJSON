1a9679af972edc1622dc4cd1f1dedae974615a63
R2boyo25 <r2boyo25@gmail.com>
2021-09-11 19:57:22 -0500

    e

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='WebsJSON',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Library to make functions for json messages over websockets',
+    author='R2Boyo25',
+    license='GPLV3',
+)
